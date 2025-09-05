56bf4f84ee911c4c9ce2f2ecf91c583b734048bc
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-09-05 20:34:48 -0300

    feat: retrieve and display detailed system information including CPU and OS details

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"system-manager/internal/hardware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	systemInfo := hardware.GetSystemInfo()
+
+	fmt.Println("Sistema operacional:", systemInfo.OS)
+	fmt.Println("Arquitetura:", systemInfo.Arch)
+	fmt.Println("Versão do SO:", systemInfo.Version)
+	fmt.Println("Informações da CPU:", "Modelo:", systemInfo.CPU.ModelName, ", Núcleos físicos:", systemInfo.CPU.Cores, ","+
+		" "+
+		"Núcleos lógicos:",
+		systemInfo.CPU.LogicsCores)
 }
