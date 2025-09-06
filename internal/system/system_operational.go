f47ea17e70428726da456dacf792229081980798
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-09-05 21:49:28 -0300

    feat: add functions to retrieve OS and architecture information

---
+++ internal/system/system_operational.go
@@ -0,0 +1,50 @@
+package system
+
+import "runtime"
+
+var systems = map[string]string{
+	"linux":   "Linux",
+	"windows": "Windows",
+	"darwin":  "macOS",
+}
+
+var architectures = map[string]string{
+	"amd64": "x86_64",
+	"386":   "x86",
+	"arm":   "ARM",
+	"arm64": "ARM64",
+}
+
+type SystemInfo struct {
+	OS      string `json:"os"`
+	Version string `json:"version"`
+	Arch    string `json:"arch"`
+}
+
+func GetOS() string {
+	osName := runtime.GOOS
+
+	for key, value := range systems {
+		if key == osName {
+			return value
+		}
+	}
+
+	return "unknown"
+}
+
+func GetArch() string {
+	arch := runtime.GOARCH
+
+	for key, value := range architectures {
+		if key == arch {
+			return value
+		}
+	}
+
+	return "unknown"
+}
+
+func GetSystemVersion() string {
+	return readVersionOs()
+}
